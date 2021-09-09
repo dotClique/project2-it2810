4ea6290e9730e7131b1ac3532d0ae4cdf32a9040
Ulrik Røsby <ulrik@rosby.no>
2021-09-09 12:12:17 +0200

    feat: Set up prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react', 'jsx-a11y', 'import'],
+  rules: {
+    'no-console': 'warn',
+    'import/first': 'error',
+    'react/prop-types': 0,
+    'linebreak-style': ['error', 'unix'],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
