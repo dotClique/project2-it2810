2f403cf051429af2e29f59884b2ea0564cb809df
Ulrik Røsby <ulrik@rosby.no>
2021-09-26 14:28:30 +0200

    feat: Add barchart for avg time per issue-label

--- src/pages/TimePerIssueLabelPage/index.tsx
+++ src/pages/TimePerIssueLabelPage/index.tsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from 'react';
+import ChartBar from '../../components/ChartBar/index';
 import PageContainer from '../../components/PageContainer';
 import { getIssuesFromAPI } from '../../helpers/api-calls';
-import { Issue } from '../../helpers/types';
+import { BarDataItem } from '../../helpers/types';
+import { avgTimePerIssueLabel } from '../../helpers/utils';
 
 export default function TimePerIssueLabelPage() {
-  const [state, setState] = useState<Issue[] | null>(null);
+  const [data, setData] = useState<BarDataItem[] | null>(null);
 
   useEffect(() => {
     getIssuesFromAPI().then((res) => {
       if (!res.ok) return console.log(res.status, res.data);
-      setState(res.data);
+      setData(avgTimePerIssueLabel(res.data));
     });
   }, []);
 
   return (
-    <PageContainer title="Average Time Per Issue-label">
-      <div>
-        <p>Something</p>
-        <pre>{JSON.stringify(state ?? {}, null, 2)}</pre>
-      </div>
+    <PageContainer title="Average  Close Time Per Issue-label">
+      <p>
+        This counts only finished issues and it is the total time from the issue was created to it
+        was closed.
+      </p>
+      <div>{data && <ChartBar data={data} title="Average close time per issue label" />}</div>
     </PageContainer>
   );
 }
