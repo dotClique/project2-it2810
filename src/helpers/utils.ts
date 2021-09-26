7fc1217b191a24c372c0b314b28a990201cc2170
Ulrik Røsby <ulrik@rosby.no>
2021-09-26 14:20:51 +0200

    feat: Add function to find the average time per issue-label

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,5 @@
+import { BarDataItem, Issue } from './types';
+
 /**
  * Gets the environmental variable with envName and throws error if not found.
  * @param envName The envinmental varaible to get.
@@ -8,4 +10,50 @@ export const getEnv = (envName: string): string => {
   const variable = process.env[envName];
   if (!variable) throw new Error(`Environmental variable ${envName} not found`);
   return variable;
+};
+
+/**
+ * Gets the average time used per issue label.
+ * @param issues The issues to do the calculation on.
+ * @returns A list of BarDataItems to be used to show in the ChartBar component.
+ */
+export const avgTimePerIssueLabel = (issues: Issue[]): BarDataItem[] => {
+  // Find the labels
+  type LabelData = {
+    label: string;
+    totalTime: number; // Time in seconds from start to close
+    issueCount: number;
+  };
+  const labels: string[] = [];
+  const labelDataList: LabelData[] = [];
+  issues.forEach((issue) => {
+    // Add the labels in the issue to the labels list if they are not already added
+    // Also intiate the labelDataList with those labels
+    issue.labels.forEach((thisLabel) => {
+      if (labels.indexOf(thisLabel) === -1) {
+        labels.push(thisLabel);
+        labelDataList.push({ label: thisLabel, totalTime: 0, issueCount: 0 });
+      }
+    });
+    // Add the time from the issue was created to it was closed (if it is closed)
+    if (issue.closed_at == null) return;
+    const start = new Date(issue.created_at);
+    const end = new Date(issue.closed_at);
+    const hoursUsed = (end.getTime() - start.getTime()) / (1000 * 60 * 60); // end-start gives time different in milliseconds, converting to hours
+    // Update the relevant values of labelDateList with the new time and issue
+    labelDataList.forEach((labelData) => {
+      if (issue.labels.indexOf(labelData.label) > -1) {
+        labelData.totalTime += hoursUsed;
+        labelData.issueCount += 1;
+      }
+    });
+  });
+  // Convert the labelDataList array to a BarDataItem array by averaging the totalTime by issueCount
+  const barDataItems: BarDataItem[] = labelDataList.map((labelData) => {
+    return {
+      barLabel: labelData.label,
+      barValue: labelData.totalTime / labelData.issueCount,
+    };
+  });
+  return barDataItems;
 };
