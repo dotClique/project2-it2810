cf0b75b9fd86ba907cd72845bdcc0308bfe6fb4f
Ulrik Røsby <ulrik@rosby.no>
2021-09-26 13:42:45 +0200

    feat: Add type for Issue

--- src/pages/TimePerIssueLabelPage/index.tsx
+++ src/pages/TimePerIssueLabelPage/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import PageContainer from '../../components/PageContainer';
+import { getIssuesFromAPI } from '../../helpers/api-calls';
+import { Issue } from '../../helpers/types';
 
 export default function TimePerIssueLabelPage() {
+  const [state, setState] = useState<Issue[] | null>(null);
+
+  useEffect(() => {
+    getIssuesFromAPI().then((res) => {
+      if (!res.ok) return console.log(res.status, res.data);
+      setState(res.data);
+    });
+  }, []);
+
   return (
     <PageContainer title="Average Time Per Issue-label">
-      <div>PageContainer</div>
+      <div>
+        <p>Something</p>
+        <pre>{JSON.stringify(state ?? {}, null, 2)}</pre>
+      </div>
     </PageContainer>
   );
 }
