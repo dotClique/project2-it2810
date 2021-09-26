a064ca56c560682fef313d816aa11b564ecdb758
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-09-26 19:06:24 +0200

    feat: add recursive api call of commits

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,6 +1,7 @@
 export type APIResponse<ResponseType> = {
   status: number;
   ok: boolean;
+  headers: ResponseType;
   data: ResponseType & ErrorResponse;
 };
 
@@ -9,3 +10,19 @@ export type ErrorResponse = {
 };
 
 export type APIRequestMethods = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+export type commit = {
+  id: string;
+  short_id: string;
+  title: string;
+  author_name: string;
+  author_email: string;
+  authored_date: string;
+  committer_name: string;
+  committer_email: string;
+  committed_date: string;
+  created_at: string;
+  message: string;
+  parent_ids: string[];
+  web_url: string;
+};
