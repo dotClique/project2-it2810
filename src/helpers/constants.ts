bf609223486fd473bb08d2b0344d61b7b3f14ea1
Ulrik Røsby <ulrik@rosby.no>
2021-09-27 12:18:00 +0200

    feat: Add paramterization of the labels

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,1 +1,42 @@
 export const PROJECT_ID = 11758;
+
+/**
+ * The labels used by the group in GitLab
+ */
+export enum LABELS {
+  BROWSER_STORAGE = 'Browser storage',
+  HARD = 'Hard',
+  DOING = 'Doing',
+  IMPORTANT = 'Important',
+  API = 'API',
+  POSSIBLY_IMPOSSIBLE = 'Possibly Impossible',
+  EASY = 'Easy',
+  GOOD_FIRST = 'Good first issue',
+  SET_UP = 'Set-up',
+  TESTING = 'Testing',
+  DEV_OPS = 'DevOps',
+  DOC = 'Documentation',
+  PLANNING = 'Planning',
+}
+
+// Categorizing the labels
+
+export const allLabels = Object.values(LABELS);
+
+export const difficultyLabels = [
+  LABELS.GOOD_FIRST,
+  LABELS.EASY,
+  LABELS.HARD,
+  LABELS.POSSIBLY_IMPOSSIBLE,
+];
+export const taskDescriptionLabels = [
+  LABELS.API,
+  LABELS.BROWSER_STORAGE,
+  LABELS.DEV_OPS,
+  LABELS.DOC,
+  LABELS.SET_UP,
+  LABELS.TESTING,
+];
+export const otherLabels = allLabels.filter(
+  (el) => difficultyLabels.indexOf(el) == -1 && taskDescriptionLabels.indexOf(el) == -1,
+);
--- src/pages/TimePerIssueLabelPage/index.tsx
+++ src/pages/TimePerIssueLabelPage/index.tsx
@@ -1,19 +1,43 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import ChartBar from '../../components/ChartBar/index';
 import PageContainer from '../../components/PageContainer';
 import { getIssuesFromAPI } from '../../helpers/api-calls';
-import { BarDataItem } from '../../helpers/types';
+import {
+  difficultyLabels,
+  LABELS,
+  otherLabels,
+  taskDescriptionLabels,
+} from '../../helpers/constants';
+import { BarDataItem, Issue } from '../../helpers/types';
 import { avgTimePerIssueLabel } from '../../helpers/utils';
+import useStyles from './styles';
 
 export default function TimePerIssueLabelPage() {
+  const [allIssueData, setAllIssueData] = useState<Issue[] | null>(null);
   const [data, setData] = useState<BarDataItem[] | null>(null);
+  const [selected, setSelected] = useState<LABELS[]>(difficultyLabels);
+  const classes = useStyles();
 
+  // On page load, get the issues from the API and load it into a state.
   useEffect(() => {
     getIssuesFromAPI().then((res) => {
-      if (!res.ok) return console.log(res.status, res.data);
-      setData(avgTimePerIssueLabel(res.data));
+      // eslint-disable-next-line
+      if (!res.ok) return console.error(res.status, res.data);
+      setAllIssueData(res.data);
     });
   }, []);
+
+  // Show data of selected labels when selected changes
+  useEffect(() => {
+    // If allIssueData or selected is null, something is not selected and or the data is not
+    if (allIssueData == null) return;
+    if (selected == null) return;
+
+    // Find the average time used to close an issue with one of the selected labels
+    const dataForRelevantIssues = avgTimePerIssueLabel(allIssueData, selected);
+    setData(dataForRelevantIssues);
+  }, [selected, allIssueData]);
 
   return (
     <PageContainer title="Average  Close Time Per Issue-label">
@@ -21,7 +45,31 @@ export default function TimePerIssueLabelPage() {
         This counts only finished issues and it is the total time from the issue was created to it
         was closed.
       </p>
-      <div>{data && <ChartBar data={data} title="Average close time per issue label" />}</div>
+      <div>
+        <FormControl className={classes.dropdown}>
+          <InputLabel id="demo-simple-select-label">Showing labels</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selected}
+            onChange={(e) => {
+              const newValue = e.target.value as LABELS[];
+              setSelected(newValue);
+            }}
+          >
+            <MenuItem key={JSON.stringify(difficultyLabels)} value={difficultyLabels}>
+              <div>Difficulty</div>
+            </MenuItem>
+            <MenuItem key={JSON.stringify(taskDescriptionLabels)} value={taskDescriptionLabels}>
+              <div>Task Description</div>
+            </MenuItem>
+            <MenuItem key={JSON.stringify(otherLabels)} value={otherLabels}>
+              <div>Other</div>
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {data && <ChartBar data={data} title="Average close time per issue label" />}
+      </div>
     </PageContainer>
   );
 }
