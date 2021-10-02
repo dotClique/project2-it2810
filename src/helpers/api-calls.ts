17220ce2c714cdde5f5ec98f0cc6a7721a65b587
Birk Gustav Samson Stoveland <bgstovel@stud.ntnu.no>
2021-10-02 12:20:47 +0200

    feat: Add getCommitByBranchFromApi and getAllCommitsByBranchFromApi api-calls

--- src/helpers/api-calls.ts
+++ src/helpers/api-calls.ts
@@ -1,5 +1,5 @@
 import { PROJECT_ID } from './constants';
-import { APIRequestMethods, APIResponse, Commit } from './types';
+import { APIRequestMethods, APIResponse, Branch, Commit } from './types';
 import { getEnv } from './utils';
 
 /**
@@ -69,6 +69,29 @@ const getBranchesFromApi = async (data: Array<Branch>, page: number) => {
       }
     }
   });
+};
+
+const getCommitByBranchFromApi = async (data: Array<Commit>, page: number, branchName: string) => {
+  return fromAPI(
+    '/repository/commits?per_page=101000&page=' + page + '&ref_name=' + branchName,
+    'GET',
+  ).then(async (res) => {
+    if (res.ok) {
+      data = data.concat(res.data);
+      if (res.headers.get('x-next-page') > page) {
+        await getCommitByBranchFromApi(data, page + 1, branchName).then((res_data) => {
+          return res_data;
+        });
+      } else {
+        return data;
+      }
+    }
+  });
+};
+
+export const getAllCommitsByBranchFromAPI = async (branchName: string) => {
+  let data: Commit[] = [];
+  return getCommitByBranchFromApi(data, 1, branchName);
 };
 
 export const getAllBranchesFromAPI = async () => {
