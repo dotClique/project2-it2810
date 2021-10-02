22361bbffad9a7f6a716061253f75cb6c441ff23
Birk Gustav Samson Stoveland <bgstovel@stud.ntnu.no>
2021-10-02 22:40:22 +0200

    feat: reworked to be based on merge request instead of branch

--- src/pages/CommitsPerBranch/index.tsx
+++ src/pages/CommitsPerBranch/index.tsx
@@ -1,9 +1,13 @@
 import PageContainer from '../../components/PageContainer/index';
 import ChartBar from '../../components/ChartBar';
-import { getAllBranchesFromAPI, getAllCommitsByBranchFromAPI } from '../../helpers/api-calls';
+import {
+  getAllMergeRequestsFromAPI,
+  getAllCommitsByMergeRequestFromAPI,
+} from '../../helpers/api-calls';
 import { useEffect, useState } from 'react';
-import { Commit } from '../../helpers/types';
+import { Branch, Commit, MergeRequest } from '../../helpers/types';
 import { Switch } from '@material-ui/core/';
+import { stringify } from 'querystring';
 
 export default function CommitsPerBranchPage() {
   const [data, setData] = useState<Array<{ barLabel: string; barValue: number }> | null>(null);
@@ -14,29 +18,26 @@ export default function CommitsPerBranchPage() {
   const [activeBranch, setActiveBranch] = useState<Map<string, boolean> | null>(null);
 
   useEffect(() => {
-    getAllBranchesFromAPI().then((res) => {
+    getAllMergeRequestsFromAPI().then((res) => {
       if (res) {
-        getAllCommitsByBranchFromAPI(res).then((res2) => {
+        getAllCommitsByMergeRequestFromAPI(res).then((res2) => {
           if (res2) {
             const activeBranches = new Map<string, boolean>();
-            res2.forEach((value: Array<Commit>, key: string) => {
-              activeBranches.set(key.slice(0, 30), true);
-              if (key !== 'master') {
-                const master: Commit[] = res2.get('master') || new Array<Commit>();
-                for (let j = 0; j < value.length; j++) {
-                  master.forEach((masterCommit: Commit) => {
-                    if (masterCommit.id == value[j].id) {
-                      (res2.get(key) as Array<Commit>).splice(j, 1);
-                      j--;
-                      return;
-                    }
-                  });
-                }
-              }
-              if (!commitsByBranch.includes({ barLabel: key, barValue: value.length })) {
-                commitsByBranch.push({ barLabel: key.slice(0, 30), barValue: value.length });
+            res2.forEach((value: Array<Commit>, key: number) => {
+              activeBranches.set(String(key) as string, true);
+              if (
+                !commitsByBranch.includes({
+                  barLabel: String(key) as string,
+                  barValue: value.length,
+                })
+              ) {
+                commitsByBranch.push({
+                  barLabel: String(key) as string,
+                  barValue: value.length,
+                });
               }
             });
+            commitsByBranch.reverse();
             setData(commitsByBranch);
             setTrueData(commitsByBranch);
             setActiveBranch(activeBranches);
@@ -70,7 +71,7 @@ export default function CommitsPerBranchPage() {
                       setData(temp_list);
                     }}
                   />
-                  {m.barLabel}
+                  Merge Request no. {m.barLabel}
                 </div>
               );
             }
